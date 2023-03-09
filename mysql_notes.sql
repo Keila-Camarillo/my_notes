23373d712e5d38c531da5a90a25fc921af4d1e04
Keila-Camarillo <keila.a.camarillo@gmail.com>
2023-03-09 09:08:59 -0600

    mysql notes

---
+++ mysql_notes.sql
@@ -0,0 +1,17 @@
+SHOW DATABASES;
+
+USE 311_data; -- select database
+
+SELECT database(); -- select db
+
+show tables; -- view tables
+
+show create table dept; -- show how table was created 
+-- unsigned = positive numbers & signed = negative numbers
+
+
+
+
+
+
+
